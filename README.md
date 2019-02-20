40402974877c206fcd30a8330c942a8eaa0f2569
karenorman <karenorman@haileyde-mbp.lan>
2019-02-21 00:03:18 +0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # gcp_translate
 Cloud Function for Google Translate
+
+# Request Parameter
+Content-Type : "application/json"
+
+```Body 
+{
+    text : TEXT_TO_BE_TRANSLATE,
+    lang : "zh-TW" (Default, optional) 
+}
+```
+
+Response
+```
+{
+    code : "000",
+    message : The translated message
+}
+```
+Failure case
+If no text passed : Bad request
+
+If the image is not contains any text
+```{
+    code : "099",
+    message : "URL do not contains any text"
+}
+```
+
+
+# Setup
+Require a .env file and credential.json which is download from Google Translate API
+
+* .env
+* GOOGLE_APPLICATION_CREDENTIALS=credential.json
+```
+credential.json
+{
+    "type": "service_account",
+    "project_id": "",
+    "private_key_id": "",
+    "private_key": "-----BEGIN PRIVATE KEY-----\\nDn6T1LbNI8Z+Wwc7z52tY=\n-----END PRIVATE KEY-----\n",
+    "client_email": "xxxxx@appspot.gserviceaccount.com",
+    "client_id": "xxxx",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/%40appspot.gserviceaccount.com"
+}
+```
+# Deployment
+* Local Simulator  
+    functions deploy handle --trigger-http
+* Read Log
+    functions logs read
---
+++ index.js
@@ -0,0 +1,63 @@
+require('dotenv').config();
+
+// Imports the Google Cloud client library
+const {
+  Translate
+} = require('@google-cloud/translate');
+
+let GCP_CLIENT = null; // Lazy Initialzation
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.handle = (req, res) => {
+
+  var text = req.body.text;
+  var lang = req.body.lang;
+  if (text == null) res.end("Bad request");
+  if (lang == null) lang = "zh-TW";
+
+  // Initialize the GCP Client if necessary
+  let client = getGCPClient();
+
+  client.translate(text, lang)
+    .then(results => {
+      const translation = results[0];
+
+      console.log(`Text: ${text}`);
+      console.log(`Translation: ${translation}`);
+
+      if (results.error != null) {
+        response = {
+          code: results.error.code.toString(),
+          message: results.error.message
+        };
+      } else {
+        response = {
+          code: "000",
+          message: translation.replace(/(\r\n|\n|\r)/gm, " "),
+          raw: translation
+        }
+      }
+
+      // Send Response
+      res.status(200).send(JSON.stringify(response));
+
+    })
+    .catch(err => {
+      res.end(`Unknown error :  ${err}`);
+
+    });
+}
+
+function getGCPClient() {
+  if (GCP_CLIENT == null) {
+    console.log("=============================================================");
+    console.log("Google Application Credentials : " + process.env.GOOGLE_APPLICATION_CREDENTIALS);
+    GCP_CLIENT = new Translate();
+    console.log("=============================================================");
+  }
+  return GCP_CLIENT;
+}
